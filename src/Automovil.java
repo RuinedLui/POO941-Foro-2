48a74729a32116d1c87013c4623ae41abeae14d1
Nivriixd <audio.studios.2021@gmail.com>
2026-09-09 20:35:44 -0600

    Update

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,5 +1,3 @@
-package automundosa;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,7 +7,6 @@ package automundosa;
  *
  * @author luise
  */
-
 public class Automovil extends Vehiculo {
     private int cantidadPuertas;
     private String tipoCombustible;
