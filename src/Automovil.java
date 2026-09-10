88ee434c1c71198e2ff1e2ace8962ed1e12e4f87
Nivriixd <audio.studios.2021@gmail.com>
2026-09-09 18:27:52 -0600

    Fix
    fix

--- src/Automovil.java
+++ src/Automovil.java
@@ -10,71 +10,37 @@ package automundosa;
  * @author luise
  */
 
-public class Vehiculo {
+public class Automovil extends Vehiculo {
+    private int cantidadPuertas;
+    private String tipoCombustible;
 
-    // Atributos comunes a todos los tipos de vehiculo (ver Tabla 1)
-    private String codigo;
-    private String marca;
-    private String modelo;
-    private int anio;
-    private double precio;
-
-    // Constructor
-    public Vehiculo(String codigo, String marca, String modelo, int anio, double precio) {
-        this.codigo = codigo;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.anio = anio;
-        this.precio = precio;
-    }
-
-    // Getters y Setters
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public int getAnio() {
-        return anio;
-    }
-
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
+    public Automovil(String codigo, String marca, String modelo, int anio, double precio,
+                     int cantidadPuertas, String tipoCombustible) {
+        super(codigo, marca, modelo, anio, precio);
+        this.cantidadPuertas = cantidadPuertas;
+        this.tipoCombustible = tipoCombustible;
     }
 
     @Override
     public String toString() {
-        return "Codigo: " + codigo +
-                "\nMarca: " + marca +
-                "\nModelo: " + modelo +
-                "\nAnio: " + anio +
-                "\nPrecio: $" + precio;
+        return super.toString() +
+                "\nCantidad de puertas: " + cantidadPuertas +
+                "\nTipo de combustible: " + tipoCombustible;
+    }
+
+    public int getCantidadPuertas() {
+        return cantidadPuertas;
+    }
+
+    public void setCantidadPuertas(int cantidadPuertas) {
+        this.cantidadPuertas = cantidadPuertas;
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
     }
 }
